4375a136060bc630b73dbe292d3aa8b50f21ef1d
Evan Waldmann <waldmannevan@knights.ucf.edu>
2019-02-14 11:19:25 -0500

    half working part 3?

--- map hw 2/map/num_methods.py
+++ map hw 2/map/num_methods.py
@@ -8,25 +8,25 @@ import scipy.sparse as sp
 import scipy.sparse.linalg as spa
 import math
 
-n = 100 #n = 1/h
-epsarr = [1e-1,1e-2 ,1e-3, 1e-4]
-eps = .001
-print(eps)
-
-# f and F vector
-def f(x):return 0
-rg= np.arange(0.01,.99 , .01)
-F = [(f(0))]+ [f(i) for i in rg ] + [f(1)- 1/.01/.01]
-print("F: ")
-print(F)
+n = 100
+epsarr = [.1,.01 ,.001, .0001]
+h=1/(n)
 
 U= []
 count =0
 for eps in epsarr:
+
+    # f and F vector
+    def f(x):return 0
+    rg= np.arange(0.01,.99 , .01)
+    F = [(f(0))]+ [f(i) for i in rg ] + [f(1)+ eps/h/h+1/2/h]
+    print("F: ")
+    print(F)
+
     #make A tridiagonal matrix
-    k = np.array([(2 - 1/eps /n)* np.ones(n-1) ,-4*np.ones(n),(2 + 1/eps /n)*np.ones(n-1)])
+    k = np.array([(eps - h/2)* np.ones(n-1) ,-2*eps*np.ones(n),(eps + h/2)*np.ones(n-1)])
     offset = [-1,0,1]
-    A =  1* eps* n*n /2 * sp.diags(k,offset).toarray()
+    A =  - 1/h/h * sp.diags(k,offset).toarray()
     print("A:")
     print(A)
 
@@ -41,45 +41,48 @@ for eps in epsarr:
 #plotting
 plt.figure()
 x =np.arange(0,1 , .01)
-plt.plot(x, U[0], 'b', lw=2, label='U b e-1')
-plt.plot(x, U[1], 'r', lw=2, label='U b e-2')
-plt.plot(x, U[2], 'g', lw=2, label='U b e-3')
 plt.plot(x, U[3], 'pink', lw=2, label='U b e-4')
+plt.plot(x, U[2], 'g', lw=2, label='U b e-3')
+plt.plot(x, U[1], 'r', lw=2, label='U b e-2')
+plt.plot(x, U[0], 'b', lw=2, label='U b e-1')
 plt.legend(loc='best')
 plt.show()
 
+print("\n\n\n")
 print("Scheme from part b: ")
 
 U1= []
 count =0
 for eps in epsarr:
+    # f and F vector
+    print(eps)
+    def f1(x):return 0
+    rg= np.arange(0.01,.99 , .01)
+    F1 = [(f1(0))]+ [f1(i) for i in rg ] + [f1(1)+ eps/(h*h) + 1/2/h]
+    print("F1: ")
+    print(F1)
+
     #make A tridiagonal matrix
-    k = np.array([(2- 1/eps /n)* np.ones(n-1) ,(-4 + 1/n/eps)*np.ones(n),(2)*np.ones(n-1)])
+    # k = np.array([(eps - h/2)* np.ones(n-1) ,-2*eps*np.ones(n),(eps + h/2)*np.ones(n-1)])
+    k = np.array([  (eps - h)* np.ones(n-1) ,(-2*eps + h) *np.ones(n), (eps)*np.ones(n-1)])
+    # k = np.array([  (eps)* np.ones(n-1) ,(-2*eps+h) *np.ones(n), (eps - h)*np.ones(n-1)])
     offset = [-1,0,1]
-    A = 1* eps* n*n * sp.diags(k,offset).toarray()
+    A =  - 1/(h*h) * sp.diags(k,offset).toarray()
     print("A:")
     print(A)
 
     print()
-
-    # # f and F vector
-    # def f(x):return 0
-    # rg= np.arange(0.01,.99 , .01)
-    # F = [(f(0))]+ [f(i) for i in rg ] + [f(1)- 1*n*n]
-    # print("F: ")
-    # print(F)
-
     #solve system to get U
-    U1.append( spa.spsolve(A, F))
+    U1.append( spa.spsolve(A, F1))
     print("U: ")
     print(U1[count])
     count = count + 1
 
 #plotting
 plt.figure()
-plt.plot(x, U1[0], 'b', lw=2, label='U b e-1')
-plt.plot(x, U1[1], 'r', lw=2, label='U b e-2')
-plt.plot(x, U1[2], 'g', lw=2, label='U b e-3')
 plt.plot(x, U1[3], 'pink', lw=2, label='U b e-4')
+plt.plot(x, U1[2], 'g', lw=2, label='U b e-3')
+plt.plot(x, U1[1], 'r', lw=2, label='U b e-2')
+plt.plot(x, U1[0], 'b', lw=2, label='U b e-1')
 plt.legend(loc='best')
 plt.show()
