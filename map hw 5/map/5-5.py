20a69d6c5c69cbb1fb1e3878d53358cf5a20bccf
Evan Waldmann <waldmannevan@knights.ucf.edu>
2019-04-03 18:21:17 -0400

    fixed 5.5 code

--- map hw 5/map/5-5.py
+++ map hw 5/map/5-5.py
@@ -1,85 +1,74 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.sparse as sp
-import scipy.linalg as sla
 import math
-from numpy.linalg import *
 
-def Euler_Method( y0, y1,  x0,  h, steps ):
+
+def Euler_Method( y0,  x0,  h, steps, f ):
     steps = steps -1
     y000=[]
-    y111=[]
     while ( steps >= 0 ):
-        y0 = y0 + h * y1
-        y1 = y1 + h * -y0
+        y0 = y0 + h * f(y0)
         x0 = x0+  h
         y000.append(y0)
-        y111.append(y1)
         steps = steps-1
-    print(x0)
-    # return (y0,y1)
-    return (y000,y111)
+    return y000
 
-def Improved_Euler_Method( y0, y1,  x0,  h, steps ) :
-    def f0(y): return y
-    def f1(y): return -y
+def Improved_Euler_Method( y0,  x0,  h, steps , f) :
     h2 = 0.5 * h
     steps = steps -1
     y000=[]
-    y111=[]
     while ( steps >= 0 ):
-        k1 = h2 * f0(y1)
-        y0 = y0+  h * f0( y1 + k1)
-        kk1= h2 * f1(y0)
-        y1 = y1 + h* f1(y0+ kk1)
+        k1 = h2 * f(y0)
+        y0 = y0+  h * f( y0 + k1)
         x0 = x0+  h
         y000.append(y0)
-        y111.append(y1)
         steps = steps-1
-    return (y000,y111)
+    return y000
 
-def classical_RK(y0, y1,  x0,  h, steps):
-    def f(y): return y
-    def f1(y): return -y
+def classical_RK(y0,  x0,  h, steps, f):
     h2 = 0.5 * h;
     steps = steps-1
     y000=[]
-    y111=[]
     while ( steps >= 0 ) :
-      k1 = h * f(y1)
-      k2 = h * f( y1 + 0.5 * k1)
-      k3 = h * f( y1 + 0.5 * k2)
+      k1 = h * f(y0)
+      k2 = h * f( y0 + 0.5 * k1)
+      k3 = h * f( y0 + 0.5 * k2)
       x0 = x0+ h
-      k4 = h * f( y1 + k3)
+      k4 = h * f( y0 + k3)
       y0 = y0 + 1/6 * ( k1 + k2 + k2 + k3 + k3 + k4 )
-
-      kk1 = h * f1(y0)
-      kk2 = h * f1(y0 + 0.5 * kk1)
-      kk3 = h * f1(y0 + 0.5 * kk2)
-      kk4 = h * f1(y0 + kk3)
-      y1 = y1 + 1/6 * ( kk1 + kk2 + kk2 + kk3 + kk3 + kk4 )
-
       y000.append(y0)
-      y111.append(y1)
       steps = steps-1
-    return (y000,y111)
+    return y000
 
-n= 100
-y00=1
-y11 =0
+def getArraysFromList(y,n):
+    numArrays = len(y[1])
+    l = [None] * numArrays
+    for t in np.arange(0,numArrays) :
+        u=[i[t] for i in y]
+        l[t] = u
+    return l
+
+n= 50
 x= 0
-h= 2*math.pi/n
+h= 2*math.pi/n # (2pi -0) /n  length of the interval over the number of steps
 step= n
 
-yn1 = Euler_Method(y00, y11,x,h,step)
+y0 = np.array([1,0])
+def f(y):
+    return np.array([y[1], -y[0]])
+
+yn1 = Euler_Method(y0,x,h,step, f)
+yn1 = getArraysFromList(yn1,n)
 print("Euler_Method:")
 print(yn1)
 
-yn2 = Improved_Euler_Method(y00, y11,x,h,step)
+yn2 = Improved_Euler_Method(y0,x,h,step,f)
+yn2 = getArraysFromList(yn2,n)
 print("Improved_Euler_Method:")
 print(yn2)
 
-yn3 = classical_RK(y00, y11,x,h,step)
+yn3 = classical_RK(y0,x,h,step,f)
+yn3 = getArraysFromList(yn3,n)
 print("classical_RK:")
 print(yn3)
 
@@ -87,4 +76,12 @@ plt.figure()
 plt.plot(np.linspace(0, 2*math.pi, n), yn1[0], label= 'Euler')
 plt.plot(np.linspace(0, 2*math.pi, n), yn2[0], label= 'Improved_Euler_Method')
 plt.plot(np.linspace(0, 2*math.pi, n), yn3[0], label = "RK ")
+plt.legend(loc='best')
+plt.show()
+
+plt.figure()
+plt.plot(np.linspace(0, 2*math.pi, n), yn1[1], label= 'Euler')
+plt.plot(np.linspace(0, 2*math.pi, n), yn2[1], label= 'Improved_Euler_Method')
+plt.plot(np.linspace(0, 2*math.pi, n), yn3[1], label = "RK ")
+plt.legend(loc='best')
 plt.show()
--- map hw 5/map/5-6.py
+++ map hw 5/map/5-6.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def Euler_Method( y0,  x0,  h, steps ):
-    def f(y): return -20*y
+def Euler_Method( y0,  x0,  h, steps,f ):
     steps = steps -1
     y000=[]
     while ( steps >= 0 ):
@@ -17,14 +16,14 @@ def geth(t0,t1, num):
     points = range/num
     return (points)
 
-n=50
-
-y00=1
-x= 0
-h= geth(0,2,n)
+n=50 # number of desired steps
+y00=1 # inital y
+x= 0 # initial x
+h= geth(0,2,n) # geth(interval end ponts, number of steps)
 step= n
+def f(y): return -20*y # given function
 
-yn2 = Euler_Method(y00,x,h,step)
+yn2 = Euler_Method(y00,x,h,step, f)
 print("Euler_Method:")
 print(yn2)
 
