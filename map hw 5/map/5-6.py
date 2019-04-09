2ef29f889bbbcf5b017acb96a5ea1ea1c58306b7
waldmannly <32138744+waldmannly@users.noreply.github.com>
2019-04-08 22:16:17 -0400

    finished 5-6

--- map hw 5/map/5-6.py
+++ map hw 5/map/5-6.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def Euler_Method( y0,  x0,  h, steps,f ):
     steps = steps -1
@@ -16,17 +17,34 @@ def geth(t0,t1, num):
     points = range/num
     return (points)
 
-n=50 # number of desired steps
+# n=50 # number of desired steps
 y00=1 # inital y
 x= 0 # initial x
-h= geth(0,2,n) # geth(interval end ponts, number of steps)
-step= n
+# h= geth(0,2,n) # geth(interval end ponts, number of steps)
+# step= n
 def f(y): return -20*y # given function
 
-yn2 = Euler_Method(y00,x,h,step, f)
-print("Euler_Method:")
-print(yn2)
+step=20
+yn20 = Euler_Method(y00,x,geth(0,2,step),step, f)
+
+step=30
+yn30 = Euler_Method(y00,x,geth(0,2,step),step, f)
+
+step=50
+yn50 = Euler_Method(y00,x,geth(0,2,step),step, f)
+
+step=100
+yn100 = Euler_Method(y00,x,geth(0,2,step),step, f)
 
 plt.figure()
-plt.plot(np.linspace(0,2,n), yn2)
+plt.plot(np.linspace(0,2,20), yn20, label ="n=20")
+plt.plot(np.linspace(0,2,30), yn30, label ="n=30")
+plt.plot(np.linspace(0,2,50), yn50, label ="n=50")
+plt.plot(np.linspace(0,2,100), yn100, label ="n=100")
+plt.legend(loc='best')
 plt.show()
+
+
+print("""We can see that for small n, this method is unstable. I would
+      assume that this is caused by a large derivative from our f function. Once
+      we increase n to make h small enough, we get a reasonable result. """)
